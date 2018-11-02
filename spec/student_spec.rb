d28c56c4a770d9de9d7cbd3f01559e5da287308f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 19:50:12 +0000

    automatically backed up by learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -76,7 +76,7 @@ describe Student do
 
     describe '#col_names_for_insert' do 
       it 'return the column names when called on an instance of Student' do 
-        expect(new_student.col_names_for_insert).to include("name, grade")
+        expect(new_student.col_names_for_insert).to include("name")
       end
 
       it 'does not include an id column' do 
