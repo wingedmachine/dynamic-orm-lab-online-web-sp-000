c0dcd6e2d32b99365b2d7d6842a17d100ef120c6
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 20:30:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,37 +3,4 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  def self.column_names
-    DB[:conn].execute("PRAGMA table_info('#{table_name}')").map do |column|
-      column["name"]
-    end.compact
-  end
-
-  self.column_names.each do |column_name|
-    attr_accessor column_name.to_sym
-  end
-
-  def initialize(options = {})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-
-  def table_name_for_insert
-    self.class.table_name
-  end
-
-  def col_names_for_insert
-    col_names = self.class.column_names
-    col_names.delete("id")
-    col_names
-  end
-
-  def values_for_insert
-
-  end
 end
